0c2c4a89a98ca733dac3c97d3b503eb3058749b3
Shane McDonough <shanemcdman@gmail.com>
2021-08-02 14:33:10 -0700

    Initial

---
+++ main.py
@@ -0,0 +1,30 @@
+from typing import List
+from matplotlib import pyplot as plt
+import sys
+
+def collatz(n: int) -> List[int]:
+    if n < 1:
+        raise ValueError("n cannot start less than one")
+    def inner(n: int) -> 'generator':
+        yield n
+        if n == 1:
+            return
+        if n % 2: # odd
+            n *= 3
+            n += 1
+        else: # even
+            n //= 2
+        yield from inner(n)
+    return list(inner(n))
+
+def irange(count: int, step: int = 1) -> range:
+    return range(1, count + 1, step)
+
+def main():
+    for i in irange(20):
+        plt.plot(collatz(i), '-o')
+    plt.show()
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(2000)
+    main()
